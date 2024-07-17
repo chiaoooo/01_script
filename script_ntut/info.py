b44c1b1d2414f8afb103a84f0b47fc7cfb66fc8d
chiaoooo <62134746+chiaoooo@users.noreply.github.com>
2024-07-17 13:38:46 +0800

    Add files via upload

---
+++ script_ntut/info.py
@@ -0,0 +1,8 @@
+TOTAL_PAGES = 54
+# personal information below
+ID = "學號：___________ "  # student ID here
+NAME = "姓名：___________"  # name here
+
+# ID = "   "  # student ID here
+# NAME = "  "  # name here
+NUMBER = 1  # number here
